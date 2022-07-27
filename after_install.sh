db849c0b6ab316284415eb6c150635e3daea4aa2
Matheus Duque <mduzch@RD0085239.global.scd.scania.com>
2022-07-27 16:02:19 +0200

    v0.0.2 - Testing CodeDeploy

---
+++ after_install.sh
@@ -0,0 +1,10 @@
+
+#!/usr/bin/env bash
+
+# Install libaries
+#cd /var/www/backend
+#virtualenv -p python3 venv
+#source venv/bin/activate
+#pip install -r requirements.txt
+#python manage.py migrate
+#python manage.py collectstatic --no-input
