cc93f8465b978c027f20ef78814f14754f5f9427
Matheus Duque <mduzch@RD0085239.global.scd.scania.com>
2022-08-01 19:34:23 +0200

    v0.0.6 - Dockerfile now works.

--- djangobackend/djangobackend/settings.py
+++ djangobackend/djangobackend/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-i^_(9sxh#(twy=o&bxda+_-da3eog(3%b34gkfwub^p@gp9hvt'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 
