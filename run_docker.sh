b5acd3b211d895bbfc37c98010c8fafffcddd671
Matheus Duque <mduzch@RD0085239.global.scd.scania.com>
2022-08-05 07:52:10 +0200

    v0.1.1 - Added docker-compose.yml.

--- run_docker.sh
+++ run_docker.sh
@@ -1,4 +1,4 @@
-docker rm $(sudo docker ps -a -q)
+docker rm -f $(sudo docker ps -a -q)
 docker rmi $(sudo docker images -q)
 docker build --tag mattdq:latest .
 docker run --name mattdq -d -p 8000:8000 mattdq:latest
